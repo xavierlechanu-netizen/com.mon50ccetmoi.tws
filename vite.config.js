b93bc114c82c86570be71bf48fed2cdd72d18f5e
Xavier <xavie@example.com>
2026-06-22 22:13:53 +0200

    Update base path for user github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/com.mon50ccetmoi.tws/',
+  base: '/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
